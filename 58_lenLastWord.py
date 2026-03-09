be76aa68a649f34b3a8d318fc94caa4ecfc05503
Arnab Chakraborty <arnabchakraborty.tar.gz@gmail.com>
2026-03-09 12:40:30 -0400

    Create 58_lenLastWord.py

---
+++ 58_lenLastWord.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        i = len(s) - 1
+        while i>=0 and s[i] == ' ':
+            i-=1
+        
+        j = i
+
+        while j>=0 and s[j] != ' ':
+            j-=1
+        
+        count = i-j
+
+        return count
